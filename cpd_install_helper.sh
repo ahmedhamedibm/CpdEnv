188aebfa6cd5c57d1f507c38fa6cfbfefe62ea10
Ahmed Hamed <ahamed@li-6a31b6cc-2e6d-11b2-a85c-904fbabf1421.ibm.com>
2022-09-06 17:37:08 -0400

    helper script -P mkdir fix

--- cpd_install_helper.sh
+++ cpd_install_helper.sh
@@ -1,4 +1,4 @@
-mkdir -P ~/cpd_install_env || sudo mkdir ~/cpd_install_env
+mkdir -p ~/cpd_install_env || sudo mkdir -p ~/cpd_install_env
 curl -sSL https://raw.github.ibm.com/National-Northeast-1/Cp4d-Cpdcli-Install/master/cpd_vars.sh?token=AACTOHKDRGKDJKEKFVPZSIDDED3MA > ~/cpd_install_env/cpd_vars.sh || sudo curl -sSL https://raw.github.ibm.com/National-Northeast-1/Cp4d-Cpdcli-Install/master/cpd_vars.sh?token=AACTOHKDRGKDJKEKFVPZSIDDED3MA > ~/cpd_install_env/cpd_vars.sh
 chmod +x ~/cpd_install_env/cpd_vars.sh || sudo chmod +x ~/cpd_install_env/cpd_vars.sh
 curl -sSL https://raw.github.ibm.com/National-Northeast-1/Cp4d-Cpdcli-Install/master/cpd_install.sh?token=AACTOHMUMMPN65V24A2OPC3DED3S2 > ~/cpd_install_env/cpd_install.sh || sudo curl -sSL https://raw.github.ibm.com/National-Northeast-1/Cp4d-Cpdcli-Install/master/cpd_install.sh?token=AACTOHMUMMPN65V24A2OPC3DED3S2 > ~/cpd_install_env/cpd_install.sh
