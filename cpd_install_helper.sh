007ae0d827852db9dc7c6ca6307c2a4f24c2677f
Ahmed Hamed <ahamed@li-6a31b6cc-2e6d-11b2-a85c-904fbabf1421.ibm.com>
2022-09-08 13:35:27 -0400

    Comments for helper script

--- cpd_install_helper.sh
+++ cpd_install_helper.sh
@@ -1,7 +1,14 @@
+# Create working directory
 mkdir -p ~/cpd_install_env || sudo mkdir -p ~/cpd_install_env
+
+# Curl and create cpd_vars.sh as executable
 curl -sSL https://raw.github.ibm.com/National-Northeast-1/Cp4d-Cpdcli-Install/master/cpd_vars.sh?token=AACTOHIAAR4RK3XPHVBG2MDDEIBP6 > ~/cpd_install_env/cpd_vars.sh || sudo curl -sSL https://raw.github.ibm.com/National-Northeast-1/Cp4d-Cpdcli-Install/master/cpd_vars.sh?token=AACTOHIAAR4RK3XPHVBG2MDDEIBP6 > ~/cpd_install_env/cpd_vars.sh
 chmod +x ~/cpd_install_env/cpd_vars.sh || sudo chmod +x ~/cpd_install_env/cpd_vars.sh
-curl -sSL https://raw.github.ibm.com/National-Northeast-1/Cp4d-Cpdcli-Install/master/cpd_install.sh?token=AACTOHJANV4JH6Z5TRQH3BTDEIBPW > ~/cpd_install_env/cpd_install.sh || sudo curl -sSL https://raw.github.ibm.com/National-Northeast-1/Cp4d-Cpdcli-Install/master/cpd_install.sh?token=AACTOHJANV4JH6Z5TRQH3BTDEIBPW > ~/cpd_install_env/cpd_install.sh
+
+# Curl and create cpd_install.sh as executable
+curl -sSL https://raw.github.ibm.com/National-Northeast-1/Cp4d-Cpdcli-Install/master/cpd_install.sh?token=AACTOHL3LMKXK54PKKNQWJDDENQ3S > ~/cpd_install_env/cpd_install.sh || sudo curl -sSL https://raw.github.ibm.com/National-Northeast-1/Cp4d-Cpdcli-Install/master/cpd_install.sh?token=AACTOHL3LMKXK54PKKNQWJDDENQ3S > ~/cpd_install_env/cpd_install.sh
 chmod +x ~/cpd_install_env/cpd_install.sh || sudo chmod +x ~/cpd_install.sh
-curl -sSL https://raw.github.ibm.com/National-Northeast-1/Cp4d-Cpdcli-Install/master/cpd_install_bastion.sh?token=AACTOHOVDIEPKUVGGW2IOWTDEICFG > ~/cpd_install_env/cpd_install_bastion.sh || sudo curl -sSL https://raw.github.ibm.com/National-Northeast-1/Cp4d-Cpdcli-Install/master/cpd_install_bastion.sh?token=AACTOHOVDIEPKUVGGW2IOWTDEICFG > ~/cpd_install_env/cpd_install_bastion.sh
+
+# Curl and create cpd_install_bastion.sh as executable
+curl -sSL https://raw.github.ibm.com/National-Northeast-1/Cp4d-Cpdcli-Install/master/cpd_install_bastion.sh?token=AACTOHLSWLLJN2Y4RFE2WGDDENQXC > ~/cpd_install_env/cpd_install_bastion.sh || sudo curl -sSL https://raw.github.ibm.com/National-Northeast-1/Cp4d-Cpdcli-Install/master/cpd_install_bastion.sh?token=AACTOHLSWLLJN2Y4RFE2WGDDENQXC > ~/cpd_install_env/cpd_install_bastion.sh
 chmod +x ~/cpd_install_env/cpd_install_bastion.sh || sudo chmod +x ~/cpd_install_env/cpd_install_bastion.sh
